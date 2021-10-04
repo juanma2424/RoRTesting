5017b742a79d0bab212a88382f10e1a9701840cf
juanma <juanmafisolis242@gmail.com>
2021-10-04 14:58:37 -0600

    CRUD DONE

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,43 @@
 class EmployeesController < ApplicationController
+ before_action :set_employee, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new employee_params
+    @employee.save
+    redirect_to @employee
+  end
+
+  def show
+  end
+
+  def index
+    @allEmployees = Employee.all
+  end
+
+  def edit
+  end
+
+  def update
+    @employee.update employee_params
+    redirect_to @employee
+  end
+
+  def destroy
+    @employee.destroy
+    redirect_to employees_path
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :lastname, :address, :phone, :salary)
+  end
+
+  def set_employee
+    @employee = Employee.find params[:id]
+  end
 end
